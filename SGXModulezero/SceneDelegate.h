edd91100aecc3a3f04b7e3bee8b57d483744a2db
rlfqiop <rlfqiop@dingtalk.com>
2022-01-29 12:37:17 +0800

    Initial Commit

---
+++ SGXModulezero/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SGXModulezero
+//
+//  Created by SGX-iMac on 2022/1/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ SGXModulezero/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SGXModulezero
+//
+//  Created by SGX-iMac on 2022/1/29.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
