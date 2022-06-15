1a7fe6516d6827f4101ba646e0eef50a48d37381
PengMr <840354825@qq.com>
2022-06-15 10:42:38 +0800

    Revert "first commit"
    This reverts commit fd82661e7f56da3beeb3cba3a8fd0727073525bb.

--- Pods/Target Support Files/Pods-SGXModulezero/Pods-SGXModulezero-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SGXModulezero : NSObject
-@end
-@implementation PodsDummy_Pods_SGXModulezero
-@end
--- upload.sh
+++
@@ -1,1 +0,0 @@
-pod repo push SGXSpecs SGXModulezero.podspec --verbose --allow-warnings --use-libraries
